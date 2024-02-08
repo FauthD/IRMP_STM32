5bf8ea3828709e11bc4cd750843af85966790f34
fauthd <19874899+FauthD@users.noreply.github.com>
2024-02-08 13:29:46 +0100

    Improve cmd lind of IrmpSend.py

--- Examples/Python/IrmpSend.py
+++ Examples/Python/IrmpSend.py
@@ -3,6 +3,7 @@
 
 # A transmitter for the IRMP that uses the map file from irmplircd
 #	to translate the key names to codes
+# Currently it is not a fully replacement of irsend
 
 # Copyright (C) 2024 Dieter Fauth
 #
@@ -39,18 +40,20 @@ class Irmp(irmp.IrmpHidRaw):
 			print(f"Unknown code: '{remote} {key}'")
 
 	###############################################
-	def SendOnce(self, remote, code):
+	def ReadConfig(self):
 		try:
 			self.ReadMap(DEFAULT_MAPFILE, "IRMP")
 			self.ReadMapDir(DEFAULT_MAPDIR)
 		except IOError as ex:
 			print(ex)
 
+	###############################################
+	def SendOnce(self, remote, code, count=1):
 		try:
 			self.open()
-			# self.IrSend('rc6+', 'KEY_OK')
-			# self.IrSend('rc6', 'KEY_ESC')
-			self.IrSend(remote, code)
+			for n in range(0, count):
+				for item in code:
+					self.IrSend(remote, item)
 			self.close()
 
 		except IOError as ex:
@@ -66,36 +69,53 @@ class Irmp(irmp.IrmpHidRaw):
 
 ######################################################
 def main():
-	parser = argparse.ArgumentParser(prog='irsend.py', description='An experimental irsend for IRMP')
+	ret=0
+	parser = argparse.ArgumentParser(description='An experimental irsend for IRMP')
 	subparsers = parser.add_subparsers(dest='command', title="command", help="SEND_ONCE etc.")
 
 	parser.add_argument('-t', '--translation', action='store_true', help=f'Path to translation table. The default is {DEFAULT_MAPFILE}.', default=DEFAULT_MAPFILE)
 	parser.add_argument('-T', '--translationdir', action='store_true', help=f'Path to translation table directory. The default is {DEFAULT_MAPDIR}.', default=DEFAULT_MAPDIR)
 	parser.add_argument('-d', '--device', action='store_true', help=f'The input device e.g. /dev/hidraw0. The default is {irmp.DefaultIrmpDevPath}', default=irmp.DefaultIrmpDevPath)
+	parser.add_argument('-#', '--count', type=int, help=f'send command n times', default=1)
+	parser.add_argument('-v', "--version", action="version", version="%(prog)s 0.0.0")
 
+	# Is there a better way to handle case insensitivity here=
 	sendonce_parser = subparsers.add_parser("SEND_ONCE", help="")
 	sendonce_parser.add_argument('remote')
-	sendonce_parser.add_argument('code')
-	# sendonce_parser.set_defaults(func=SendOnce)
+	sendonce_parser.add_argument('code', nargs="+")
+	sendonce_parser = subparsers.add_parser("send_once", help="")
+	sendonce_parser.add_argument('remote')
+	sendonce_parser.add_argument('code', nargs="+")
 
+
+	# TODO:
 	# sendstart_parser = subparsers.add_parser("SEND_START", help="")
 	# sendstart_parser.add_argument('remote')
 	# sendstart_parser.add_argument('code')
 
-	# sendstops_parser = subparsers.add_parser("SEND_START", help="")
+	# sendstops_parser = subparsers.add_parser("SEND_STOP", help="")
 	# sendstops_parser.add_argument('remote')
 	# sendstops_parser.add_argument('code')
 
+
 	# global args
-	args = parser.parse_args()
+	try:
+		args = parser.parse_args()
+	except:
+		ret = -1
 
-	ir = Irmp(device_path=args.device, map=args.translation, mapdir=args.translationdir)
-	if args.command == 'SEND_ONCE':
-		print(f"SEND_ONCE {args.remote} {args.code}")
-		ir.SendOnce(args.remote, args.code)
+	if ret==0:
+		ir = Irmp(device_path=args.device, map=args.translation, mapdir=args.translationdir)
+		ir.ReadConfig()
 
-	return 0
+		if args.command.upper() == 'SEND_ONCE':
+			# print(f"SEND_ONCE {args.remote} {args.code}")
+			ir.SendOnce(args.remote, args.code, args.count)
+
+	return ret
 
 if __name__ == "__main__":
 	exit ( main() )
 
+# SEND_ONCE rc6 KEY_OK KEY_ESC
+# send_once rc6 KEY_OK KEY_ESC
